63dfc2ba8fb06d8ba199dd07b9ff57157d59e968
FScirE <leo.soleke@gmail.com>
2025-05-08 16:10:10 +0200

    changed tables
    todo: update er

--- Database/tables.sql
+++ Database/tables.sql
@@ -1,27 +1,38 @@
 
 CREATE TABLE Team (
-    team_id INTEGER PRIMARY KEY,
-    name VARCHAR(30),
-    region VARCHAR(30)
+    team_id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    region VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_team PRIMARY KEY (team_id)
 );
 
 CREATE TABLE Player (
-    player_id INTEGER PRIMARY KEY,
-    name VARCHAR(30),
-    team_id INTEGER,
-    country VARCHAR(30)
+    player_id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    team_id INTEGER NOT NULL,
+    country VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_player PRIMARY KEY (player_id),
+    CONSTRAINT fk_team FOREIGN KEY (team_id) REFERENCES Team(team_id)
 );
 
 CREATE TABLE Game (
-    game_id INTEGER PRIMARY KEY,
-    date DATE,
-    team_1_id INTEGER,
-    team_2_id INTEGER
+    game_id INTEGER NOT NULL AUTO_INCREMENT,
+    date DATE NOT NULL,
+    team_1_id INTEGER NOT NULL,
+    team_2_id INTEGER NOT NULL,
+    CONSTRAINT pk_pgame PRIMARY KEY (game_id),
+    CONSTRAINT fk_team_1 FOREIGN KEY (team_1_id) REFERENCES Team(team_id),
+    CONSTRAINT fk_team_2 FOREIGN KEY (team_2_id) REFERENCES Team(team_id)
 );
 
-CREATE TABLE MatchResult (
-    game_id INTEGER PRIMARY KEY,
-    winner_team_id INTEGER,
-    team_1_score INTEGER,
-    team_2_score INTEGER,
+CREATE TABLE PlayerStats (
+    game_id INTEGER NOT NULL,
+    player_id INTEGER NOT NULL,
+    kills INTEGER,
+    deaths INTEGER,
+    damage INTEGER,
+    healing INTEGER,
+    CONSTRAINT pk_stats PRIMARY KEY (game_id, player_id),
+    CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES Game(game_id),
+    CONSTRAINT fk_player FOREIGN KEY (player_id) REFERENCES Player(player_id)
 );
